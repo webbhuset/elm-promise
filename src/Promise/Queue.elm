1908ee7b1d05d6006b3595f839c5d031b80c0ccf
Albert Dahlin <info@albertdahlin.com>
2025-10-13 13:15:09 +0200

    Make Queue handle concurrency and Promises

--- src/Promise/Queue.elm
+++ src/Promise/Queue.elm
@@ -1,111 +1,231 @@
-module Promise.Queue exposing (..)
+module Promise.Queue exposing
+    ( Queue, RequestId(..)
+    , empty, add, remove, insert
+    , requests, any, all
+    , run, Group, send, withGroup
+    )
 
+{-|
+
+@docs Queue, RequestId
+
+@docs empty, add, remove, insert
+
+@docs requests, any, all
+
+@docs run, Group, send, withGroup
+
+-}
+
+import Promise exposing (Promise)
 import Promise.State as State exposing (State)
+import Set exposing (Set)
 
 
 type RequestId
-    = RequestId Int
+    = RequestId String
 
 
-type alias Queue req =
-    { requests : List ( RequestId, req )
+type Queue reqest
+    = Queue (I_Queue reqest)
+
+
+type alias I_Queue r =
+    { requests : List ( RequestId, r )
     , nextId : Int
+    , prefix : String
     }
 
 
-empty : Queue req
-empty =
+requests : Queue request -> List ( RequestId, request )
+requests (Queue queue) =
+    queue.requests
+
+
+empty : String -> Queue req
+empty prefix =
     { requests = []
     , nextId = 0
+    , prefix = prefix
     }
+        |> Queue
 
 
 add : req -> Queue req -> Queue req
-add request queue =
-    { queue
-        | requests = queue.requests ++ [ ( RequestId queue.nextId, request ) ]
-        , nextId = queue.nextId + 1
-    }
+add request (Queue queue) =
+    let
+        id =
+            (queue.prefix ++ "-" ++ String.fromInt queue.nextId)
+                |> RequestId
+    in
+    Queue
+        { queue
+            | requests =
+                queue.requests ++ [ ( id, request ) ]
+            , nextId = queue.nextId + 1
+        }
 
 
 remove : RequestId -> Queue req -> Queue req
-remove id queue =
-    { queue
-        | requests =
-            List.filter
-                (\( itemId, _ ) ->
-                    itemId /= id
-                )
-                queue.requests
-    }
+remove id (Queue queue) =
+    Queue
+        { queue
+            | requests =
+                List.filter
+                    (\( itemId, _ ) ->
+                        itemId /= id
+                    )
+                    queue.requests
+        }
 
 
 insert : RequestId -> req -> Queue req -> Queue req
-insert id newRequest queue =
-    { queue
-        | requests =
-            List.map
-                (\( itemId, request ) ->
-                    if itemId == id then
-                        ( itemId, newRequest )
+insert id newRequest (Queue queue) =
+    Queue
+        { queue
+            | requests =
+                List.map
+                    (\( itemId, request ) ->
+                        if itemId == id then
+                            ( itemId, newRequest )
 
-                    else
-                        ( itemId, request )
-                )
-                queue.requests
-    }
-
-
-type Action r eff
-    = Send ( r, eff )
-    | Next
-    | Wait
-
-
-sendNext : (RequestId -> r -> Action r eff) -> Queue r -> ( Queue r, List eff )
-sendNext toAction queue =
-    walkRequests toAction queue.requests
-        |> Tuple.mapFirst (\newRequests -> { queue | requests = newRequests })
-
-
-walkRequests :
-    (RequestId -> r -> Action r eff)
-    -> List ( RequestId, r )
-    -> ( List ( RequestId, r ), List eff )
-walkRequests toAction requests =
-    case requests of
-        [] ->
-            ( [], [] )
-
-        ( id, request ) :: rest ->
-            case toAction id request of
-                Send ( updatedRequest, eff ) ->
-                    ( ( id, updatedRequest ) :: rest
-                    , [ eff ]
+                        else
+                            ( itemId, request )
                     )
-
-                Next ->
-                    walkRequests toAction rest
-                        |> Tuple.mapFirst (\newRest -> ( id, request ) :: newRest)
-
-                Wait ->
-                    ( requests, [] )
+                    queue.requests
+        }
 
 
-firstEmpty : State e a -> ( r, eff ) -> Action r eff
-firstEmpty state pair =
+any : (request -> Bool) -> Queue request -> Bool
+any predicate (Queue queue) =
+    List.any (predicate << Tuple.second) queue.requests
+
+
+all : (request -> Bool) -> Queue request -> Bool
+all predicate (Queue queue) =
+    List.all (predicate << Tuple.second) queue.requests
+
+
+
+-- ATTEMPT QUEUE PROCESSING
+
+
+run :
+    (RequestId -> req -> Promise model eff err (Group req eff))
+    -> Queue req
+    -> Promise model eff err ( Queue req, List eff )
+run handleReq (Queue queue) =
+    queue.requests
+        |> List.foldl
+            (\( id, req ) ->
+                Promise.andThen
+                    (\( pp, groupsSending ) ->
+                        handleReq id req
+                            |> Promise.map
+                                (\group ->
+                                    case group of
+                                        SendGroup name updatedReq eff ->
+                                            if Set.member name groupsSending then
+                                                ( ( ( id, req ), [] ) :: pp
+                                                , groupsSending
+                                                )
+
+                                            else
+                                                ( ( ( id, updatedReq ), [ eff ] ) :: pp
+                                                , Set.insert name groupsSending
+                                                )
+
+                                        Send updatedReq eff ->
+                                            ( ( ( id, updatedReq ), [ eff ] ) :: pp
+                                            , groupsSending
+                                            )
+
+                                        StopGroup name ->
+                                            ( ( ( id, req ), [] ) :: pp
+                                            , Set.insert name groupsSending
+                                            )
+
+                                        Skip ->
+                                            ( ( ( id, req ), [] ) :: pp
+                                            , groupsSending
+                                            )
+                                )
+                    )
+            )
+            (Promise.succeed ( [], Set.empty ))
+        |> Promise.map
+            (Tuple.first
+                >> List.reverse
+                >> List.unzip
+                >> Tuple.mapSecond List.concat
+            )
+        |> Promise.map
+            (\( reqs, effects ) ->
+                ( Queue { queue | requests = reqs }
+                , effects
+                )
+            )
+
+
+type Group request effect
+    = SendGroup String request effect
+    | StopGroup String
+    | Send request effect
+    | Skip
+
+
+send :
+    State err response
+    -> Promise model eff err ( request, effect )
+    -> Promise model eff err (Group request effect)
+send state promise =
     case state of
         State.Empty ->
-            Send pair
+            promise
+                |> Promise.withState
+                |> Promise.map
+                    (\s ->
+                        case s of
+                            State.Done ( r, e ) ->
+                                Send r e
+
+                            _ ->
+                                Skip
+                    )
 
         State.Pending _ ->
-            Wait
+            promise
+                |> Promise.map (\_ -> Skip)
 
-        State.Stale _ ->
-            Send pair
+        _ ->
+            promise
+                |> Promise.map (\_ -> Skip)
 
-        State.Done _ ->
-            Next
 
-        State.Error _ ->
-            Next
+withGroup :
+    String
+    -> State err response
+    -> Promise model eff err ( request, effect )
+    -> Promise model eff err (Group request effect)
+withGroup group state promise =
+    case state of
+        State.Empty ->
+            promise
+                |> Promise.withState
+                |> Promise.map
+                    (\s ->
+                        case s of
+                            State.Done ( r, e ) ->
+                                SendGroup group r e
+
+                            _ ->
+                                Skip
+                    )
+
+        State.Pending _ ->
+            promise
+                |> Promise.map (\_ -> StopGroup group)
+
+        _ ->
+            promise
+                |> Promise.map (\_ -> Skip)
