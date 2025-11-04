c9eaf54800f9a9fb89a1c15f0f706ebbc4c9d24a
Albert Dahlin <info@albertdahlin.com>
2025-11-04 08:21:17 +0100

    Add some comments to Demo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Promise
+
+Work with asynchronous operations in a composable way in Elm.
+
+See the [example] folder for usage examples.
+
+
+[example]: https://github.com/webbhuset/elm-promise/tree/master/example/src
--- example/src/Demo.elm
+++ example/src/Demo.elm
@@ -1,5 +1,9 @@
 module Demo exposing (..)
 
+{-| A Demo application showcasing the use of the Elm Promise library to handle
+asynchronous data fetching with state management and integration with the Elm architecture.
+-}
+
 import Browser
 import Dict exposing (Dict)
 import Html exposing (Html)
@@ -13,10 +17,46 @@ import Promise.State as State exposing (State)
 import Task exposing (Task)
 
 
+{-| Declare a local alias for Promise to avoid having to
+repeat the full type signature everywhere.
+-}
 type alias Promise a =
     Promise.Promise Model (Cmd Msg) Http.Error a
 
 
+
+-- REMOTE CALLS
+
+
+{-| The response from our backend that we can receive through
+the `update` function.
+-}
+type Response
+    = GotToUpper String (Result Http.Error String)
+    | GotSuggestion String (Result Http.Error (List String))
+    | GotUser (Result Http.Error User)
+
+
+{-| In our Model we keep track of the state of our remote calls.
+
+Note that we use Dicts to cache results for different input values.
+
+-}
+type alias RemoteModel a =
+    { a
+        | fromRemote_ToUpper : Dict String (State Http.Error String)
+        , fromRemote_Suggestion : Dict String (State Http.Error (List String))
+        , fromRemote_User : State Http.Error User
+    }
+
+
+type alias User =
+    { name : String
+    }
+
+
+{-| A mock remote call that converts a string to uppercase. If the string starts with "error", it simulates an error response.
+-}
 fromRemote_ToUpper : String -> Promise String
 fromRemote_ToUpper str =
     (if String.startsWith "error" str then
@@ -28,7 +68,8 @@ fromRemote_ToUpper str =
         String.toUpper str
             |> Ok
     )
-        |> FromRemote_ToUpper str
+        |> GotToUpper str
+        |> FromRemote
         |> sendWithDelay 500
         |> Promise.fromValue
         |> Promise.fromEffectWhenEmpty
@@ -48,11 +89,14 @@ fromRemote_ToUpper str =
             )
 
 
+{-| A mock remote call that provides autocomplete suggestions based on the input string.
+-}
 fromRemote_Suggestion : String -> Promise (List String)
 fromRemote_Suggestion str =
     [ str ++ " 1", str ++ " 2", str ++ " 3" ]
         |> Ok
-        |> FromRemote_Suggestion str
+        |> GotSuggestion str
+        |> FromRemote
         |> sendWithDelay 1000
         |> Promise.fromValue
         |> Promise.fromEffectWhenEmpty
@@ -73,16 +117,14 @@ fromRemote_Suggestion str =
             )
 
 
-type alias User =
-    { name : String
-    }
-
-
+{-| A mock remote call that fetches user data based on the username.
+-}
 fromRemote_User : String -> Promise User
 fromRemote_User username =
     { name = username }
         |> Ok
-        |> FromRemote_User
+        |> GotUser
+        |> FromRemote
         |> sendWithDelay 500
         |> Promise.fromValue
         |> Promise.fromEffectWhenEmpty
@@ -96,7 +138,7 @@ fromRemote_User username =
 
 
 
--- PAGE
+-- THE PAGE
 
 
 type Page
@@ -104,12 +146,24 @@ type Page
     | Search SearchPage
 
 
+{-| The data needed for the Search page.
+
+Note that `suggestions` is a remote state. This enables us to show
+a loading indicator while fetching suggestions without the whole page
+being in a loading state.
+
+-}
 type alias SearchPage =
     { suggestions : State Http.Error (List String)
     , user : User
     }
 
 
+{-| Compose Promises to make your page.
+
+Should be familiar if you have worked with Json.Decode
+
+-}
 getPage : Promise Page
 getPage =
     Promise.fromModel
@@ -138,6 +192,11 @@ getPage =
         )
 
 
+{-| Resolve the current page based on the model.
+
+Will handle all pending promises and update the model accordingly.
+
+-}
 resolvePage : Model -> ( Model, Cmd Msg )
 resolvePage model =
     getPage
@@ -151,6 +210,8 @@ resolvePage model =
         |> Tuple.mapSecond Cmd.batch
 
 
+{-| Helper function to send a message back to the update function after a delay.
+-}
 sendWithDelay : Float -> Msg -> Cmd Msg
 sendWithDelay delay msg =
     Process.sleep delay
@@ -161,24 +222,26 @@ sendWithDelay delay msg =
 -- APP
 
 
-type Msg
-    = FromRemote_ToUpper String (Result Http.Error String)
-    | FromRemote_Suggestion String (Result Http.Error (List String))
-    | FromRemote_User (Result Http.Error User)
+type
+    Msg
+    -- Received a response from a remote call
+    = FromRemote Response
     | FromHtml_SearchTerm String
     | FromHtml_LoginSubmit String
-    -- Just a dummy message to show the initial state in the time travel debugger
+      -- Just a dummy message to show the initial state in the time travel debugger
     | AppStarted
 
 
+{-| Our Model contains all remote states.
+-}
 type alias Model =
-    { fromRemote_ToUpper : Dict String (State Http.Error String)
-    , fromRemote_Suggestion : Dict String (State Http.Error (List String))
-    , fromRemote_User : State Http.Error User
-    , fromHtml_Username : Maybe String
-    , fromHtml_SearchTerm : String
-    , page : State Http.Error Page
-    }
+    RemoteModel
+        { fromHtml_Username : Maybe String
+        , fromHtml_SearchTerm : String
+
+        -- The current page state. The page itself can also be in a pending/error state.
+        , page : State Http.Error Page
+        }
 
 
 initModel : Flags -> Model
@@ -213,6 +276,7 @@ init flags =
             initModel flags
     in
     ( model
+      -- Just a dummy command to show the initial state in the time travel debugger
     , sendWithDelay 0 AppStarted
     )
 
@@ -222,35 +286,41 @@ subscriptions model =
     Sub.none
 
 
+{-| We need to run `resolvePage` on every update to make sure
+that pending promises are handled.
+-}
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AppStarted ->
             resolvePage model
 
-        FromRemote_ToUpper str result ->
-            { model
-                | fromRemote_ToUpper =
-                    Dict.insert str
-                        (State.fromResult result)
-                        model.fromRemote_ToUpper
-            }
-                |> resolvePage
+        FromRemote response ->
+            -- When we receive a response, just insert it into the Model.
+            case response of
+                GotToUpper str result ->
+                    { model
+                        | fromRemote_ToUpper =
+                            Dict.insert str
+                                (State.fromResult result)
+                                model.fromRemote_ToUpper
+                    }
+                        |> resolvePage
 
-        FromRemote_Suggestion str result ->
-            { model
-                | fromRemote_Suggestion =
-                    Dict.insert str
-                        (State.fromResult result)
-                        model.fromRemote_Suggestion
-            }
-                |> resolvePage
+                GotSuggestion str result ->
+                    { model
+                        | fromRemote_Suggestion =
+                            Dict.insert str
+                                (State.fromResult result)
+                                model.fromRemote_Suggestion
+                    }
+                        |> resolvePage
 
-        FromRemote_User result ->
-            { model
-                | fromRemote_User = State.fromResult result
-            }
-                |> resolvePage
+                GotUser result ->
+                    { model
+                        | fromRemote_User = State.fromResult result
+                    }
+                        |> resolvePage
 
         FromHtml_SearchTerm str ->
             { model | fromHtml_SearchTerm = str }
@@ -277,86 +347,98 @@ document model =
 
 view : Model -> Html Msg
 view model =
-    view_State
-        (Html.text "Empty")
-        Html.main_
-        [ HA.class "page"
-        ]
-        (\page ->
-            case page of
-                Login ->
-                    [ view_LoginForm
-                    ]
-
-                Search searchPage ->
-                    [ Html.h1 [] [ Html.text ("Hello, " ++ searchPage.user.name ++ "!") ]
-                    , Html.label
-                        []
-                        [ Html.div [] [ Html.text "Search" ]
-                        , Html.input
-                            [ HA.type_ "search"
-                            , Events.onInput FromHtml_SearchTerm
-                            , HA.value model.fromHtml_SearchTerm
-                            , HA.autofocus True
+    model.page
+        |> view_State
+            (Html.text "Empty")
+            Html.main_
+            [ HA.class "page"
+            ]
+            (\page ->
+                case page of
+                    Login ->
+                        [ Html.section
+                            [ HA.class "page-login"
                             ]
-                            []
+                            view_LoginForm
                         ]
-                    , Html.h2 [] [ Html.text "Autocomplete Suggestions" ]
-                    , view_State
-                        (Html.text "Type to see suggestions...")
-                        Html.div
-                        [ HA.class "suggestions-result"
+
+                    Search searchPage ->
+                        [ Html.section
+                            [ HA.class "page-search"
+                            ]
+                            (view_SearchPage model searchPage)
                         ]
-                        (\suggestions ->
-                            List.map
-                                (\suggestion ->
-                                    Html.li
-                                        []
-                                        [ Html.text suggestion ]
-                                )
-                                suggestions
-                                |> Html.ul []
-                                |> List.singleton
-                        )
-                        searchPage.suggestions
-                    ]
-        )
-        model.page
+            )
 
 
-view_LoginForm : Html Msg
+view_LoginForm : List (Html Msg)
 view_LoginForm =
-    Html.section
-        [ HA.class "login-form" ]
-        [ Html.form
-            [ Events.preventDefaultOn "submit"
-                (Decode.at [ "target", "elements", "username", "value" ] Decode.string
-                    |> Decode.map
-                        (\username ->
-                            ( FromHtml_LoginSubmit username
-                            , True
-                            )
+    [ Html.form
+        [ Events.preventDefaultOn "submit"
+            (Decode.at [ "target", "elements", "username", "value" ] Decode.string
+                |> Decode.map
+                    (\username ->
+                        ( FromHtml_LoginSubmit username
+                        , True
                         )
-                )
-            ]
-            [ Html.label
-                []
-                [ Html.div [] [ Html.text "Username" ]
-                , Html.input
-                    [ HA.type_ "text"
-                    , HA.name "username"
-                    , HA.required True
-                    , HA.autofocus True
-                    , HA.placeholder "Enter your username"
-                    , HA.autocomplete False
-                    ]
-                    []
-                ]
-            , Html.button [ HA.type_ "submit" ] [ Html.text "Login" ]
-            ]
+                    )
+            )
         ]
+        [ Html.label
+            []
+            [ Html.div [] [ Html.text "Username" ]
+            , Html.input
+                [ HA.type_ "text"
+                , HA.name "username"
+                , HA.required True
+                , HA.autofocus True
+                , HA.placeholder "Enter your username"
+                , HA.autocomplete False
+                ]
+                []
+            ]
+        , Html.button [ HA.type_ "submit" ] [ Html.text "Login" ]
+        ]
+    ]
 
 
+view_SearchPage : Model -> SearchPage -> List (Html Msg)
+view_SearchPage model searchPage =
+    [ Html.h1 [] [ Html.text ("Hello, " ++ searchPage.user.name ++ "!") ]
+    , Html.label
+        []
+        [ Html.div [] [ Html.text "Search" ]
+        , Html.input
+            [ HA.type_ "search"
+            , Events.onInput FromHtml_SearchTerm
+            , HA.value model.fromHtml_SearchTerm
+            , HA.autofocus True
+            ]
+            []
+        ]
+    , Html.h2 [] [ Html.text "Autocomplete Suggestions" ]
+    , view_State
+        (Html.text "Type to see suggestions...")
+        Html.div
+        [ HA.class "suggestions-result"
+        ]
+        (\suggestions ->
+            List.map
+                (\suggestion ->
+                    Html.li
+                        []
+                        [ Html.text suggestion ]
+                )
+                suggestions
+                |> Html.ul []
+                |> List.singleton
+        )
+        searchPage.suggestions
+    ]
+
+
+{-| A helper function to render different states.
+-}
 view_State :
     Html msg
     -> (List (Html.Attribute msg) -> List (Html msg) -> Html msg)
@@ -407,13 +489,14 @@ css =
 }
 body {
     font-family: sans-serif;
-    padding: 1em;
 }
 main {
+    min-height: 100vh;
     display: grid;
-    gap: 1em;
-    max-width: 600px;
-    margin: auto;
+    &.state-pending {
+        justify-items: center;
+        align-items: center;
+    }
 }
 .state-empty {
     color: gray;
@@ -434,5 +517,16 @@ main {
     100% {
         opacity: 1;
     }
+}
+
+.page-login {
+    align-self: center;
+    justify-self: center;
+}
+.page-search {
+    padding: 1em;
+    display: grid;
+    gap: 1em;
+    align-content: start;
 }
 """
